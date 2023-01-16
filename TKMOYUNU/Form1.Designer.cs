9e3091183ea2dd318073517bf60b857e1699e1f8
metndurn <mtndrn123@gmail.com>
2023-01-16 19:39:44 +0300

    OyunOrnegi

---
+++ TKMOYUNU/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace TKMOYUNU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnTAS = new System.Windows.Forms.Button();
+            this.btnKAGIT = new System.Windows.Forms.Button();
+            this.btnMAKAS = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.lblskor = new System.Windows.Forms.Label();
+            this.lblgoster = new System.Windows.Forms.Label();
+            this.lblbot = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTAS
+            // 
+            this.btnTAS.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTAS.Location = new System.Drawing.Point(105, 260);
+            this.btnTAS.Name = "btnTAS";
+            this.btnTAS.Size = new System.Drawing.Size(160, 38);
+            this.btnTAS.TabIndex = 0;
+            this.btnTAS.Text = "TAS";
+            this.btnTAS.UseVisualStyleBackColor = true;
+            this.btnTAS.Click += new System.EventHandler(this.btnTAS_Click);
+            // 
+            // btnKAGIT
+            // 
+            this.btnKAGIT.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKAGIT.Location = new System.Drawing.Point(105, 316);
+            this.btnKAGIT.Name = "btnKAGIT";
+            this.btnKAGIT.Size = new System.Drawing.Size(160, 38);
+            this.btnKAGIT.TabIndex = 1;
+            this.btnKAGIT.Text = "KAGIT";
+            this.btnKAGIT.UseVisualStyleBackColor = true;
+            this.btnKAGIT.Click += new System.EventHandler(this.btnKAGIT_Click);
+            // 
+            // btnMAKAS
+            // 
+            this.btnMAKAS.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnMAKAS.Location = new System.Drawing.Point(105, 369);
+            this.btnMAKAS.Name = "btnMAKAS";
+            this.btnMAKAS.Size = new System.Drawing.Size(160, 38);
+            this.btnMAKAS.TabIndex = 2;
+            this.btnMAKAS.Text = "MAKAS";
+            this.btnMAKAS.UseVisualStyleBackColor = true;
+            this.btnMAKAS.Click += new System.EventHandler(this.btnMAKAS_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Enabled = false;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(105, 156);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 38);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "MAKAS";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.Enabled = false;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(105, 103);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(160, 38);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "KAGIT";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.Enabled = false;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(105, 49);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(160, 38);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "TAS";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // lblskor
+            // 
+            this.lblskor.AutoSize = true;
+            this.lblskor.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblskor.Location = new System.Drawing.Point(290, 316);
+            this.lblskor.Name = "lblskor";
+            this.lblskor.Size = new System.Drawing.Size(0, 33);
+            this.lblskor.TabIndex = 6;
+            // 
+            // lblgoster
+            // 
+            this.lblgoster.AutoSize = true;
+            this.lblgoster.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblgoster.Location = new System.Drawing.Point(98, 213);
+            this.lblgoster.Name = "lblgoster";
+            this.lblgoster.Size = new System.Drawing.Size(0, 37);
+            this.lblgoster.TabIndex = 7;
+            // 
+            // lblbot
+            // 
+            this.lblbot.AutoSize = true;
+            this.lblbot.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblbot.Location = new System.Drawing.Point(290, 103);
+            this.lblbot.Name = "lblbot";
+            this.lblbot.Size = new System.Drawing.Size(0, 33);
+            this.lblbot.TabIndex = 8;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(27, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 33);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Tas Kagıt Makas Oyunu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 417);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblbot);
+            this.Controls.Add(this.lblgoster);
+            this.Controls.Add(this.lblskor);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.btnMAKAS);
+            this.Controls.Add(this.btnKAGIT);
+            this.Controls.Add(this.btnTAS);
+            this.MaximumSize = new System.Drawing.Size(401, 456);
+            this.MinimumSize = new System.Drawing.Size(401, 456);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTAS;
+        private System.Windows.Forms.Button btnKAGIT;
+        private System.Windows.Forms.Button btnMAKAS;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label lblskor;
+        private System.Windows.Forms.Label lblgoster;
+        private System.Windows.Forms.Label lblbot;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TKMOYUNU/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TKMOYUNU
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int skor = 0;
+        int bot = 0;
+        private void btnTAS_Click(object sender, EventArgs e)
+        {
+            button4.BackColor = Color.White;
+            button5.BackColor = Color.White;
+            button6.BackColor = Color.White;
+            Random random = new Random();
+            int sayi;
+            sayi = random.Next(1, 4);
+            if (sayi==1)
+            {
+                lblgoster.Text = "Beraberlik";
+                button6.BackColor = Color.Yellow;
+            }
+            else if(sayi==2)
+            {
+                bot++;
+                lblbot.Text=bot.ToString();
+                lblgoster.Text = "Kaybettiniz";
+                button5.BackColor = Color.Red;
+            }
+            else if(sayi==3)
+            {
+                skor++;
+                lblskor.Text=skor.ToString();
+                lblgoster.Text = "Kazandiniz";
+                button4.BackColor = Color.Green;
+            }
+        }
+
+        private void btnKAGIT_Click(object sender, EventArgs e)
+        {
+            button4.BackColor = Color.White;
+            button5.BackColor = Color.White;
+            button6.BackColor = Color.White;
+            Random random = new Random();
+            int sayi;
+            sayi = random.Next(1, 4);
+            if (sayi==1)
+            {
+                skor++;
+                lblskor.Text=skor.ToString();
+                lblgoster.Text = "Kazandiniz";
+                button6.BackColor = Color.Yellow;
+            }
+            else if(sayi==2)
+            {
+                lblgoster.Text = "Beraberlik";
+                button5.BackColor = Color.Red;
+            }
+            else if (sayi==3)
+            {
+                bot++;
+                lblbot.Text=bot.ToString();
+                lblgoster.Text = "Kaybettiniz";
+                button4.BackColor = Color.Green;
+            }
+        }
+
+        private void btnMAKAS_Click(object sender, EventArgs e)
+        {
+            button4.BackColor = Color.White;
+            button5.BackColor = Color.White;
+            button6.BackColor = Color.White;
+            Random random = new Random();
+            int sayi;
+            sayi = random.Next(1, 4);
+            if (sayi==1)
+            {
+                bot++;
+                lblbot.Text=bot.ToString();
+                lblgoster.Text = "Kaybettiniz";
+                button6.BackColor = Color.Red;
+            }
+            else if (sayi==2)
+            {
+                skor++;
+                lblskor.Text=skor.ToString();
+                lblgoster.Text = "Kazandiniz";
+                button5.BackColor = Color.Green;
+            }
+            else if (sayi==3)
+            {
+                lblgoster.Text = "Beraberlik";
+                button4.BackColor = Color.Yellow;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (bot == 3)
+            {
+                lblgoster.Text = "Rakibiniz Kazandi";
+                btnTAS.Enabled = false;
+                btnKAGIT.Enabled = false;
+                btnMAKAS.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+            else if (skor == 3)
+            {
+                lblgoster.Text = "Siz Kazandiniz";
+                btnTAS.Enabled = false;
+                btnKAGIT.Enabled = false;
+                btnMAKAS.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+            }
+        }
+    }
+}
